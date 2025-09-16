181b890078a2e93178ba8c5b107b3068aada92ba
John Wayne Balles <johnwayneballes01@gmail.com>
2025-09-16 19:54:39 +0800

    added johnWayne page

---
+++ app/johnWayne/info.tsx
@@ -0,0 +1,7 @@
+export default function Info() {
+  return(
+    <div>
+        <h4 className="text-3xl font-bold">Hello World</h4>;
+    </div>
+  )
+}
---
+++ app/johnWayne/page.tsx
@@ -0,0 +1,10 @@
+import Info from "./info"
+export default function Page() {
+  return(
+    <div>
+      <h1 className="text-3xl font-bold">John Wayne M. Balles</h1>;
+    <Info />
+  </div>
+  )
+  
+}

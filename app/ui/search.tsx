9cde752dba252d4b63c2947f07c6c908d127bfde
John Wayne Balles <johnwayneballes01@gmail.com>
2025-12-02 18:32:43 +0800

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,18 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
-
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-    }, 300);
+  
   function handleSearch(term: string) {
     
   }

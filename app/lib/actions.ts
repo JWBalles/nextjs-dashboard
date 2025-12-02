8108ca3e119271122814262f926f2fbdce6839a6
John Wayne Balles <johnwayneballes01@gmail.com>
2025-12-02 19:26:00 +0800

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,7 +72,7 @@ export async function updateInvoice(
   }
 }
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
